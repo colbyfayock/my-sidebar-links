3364a95d64a1480fa2b66a20d02f4be150bdd095
Colby Fayock <fayock@gmail.com>
2024-02-22 11:32:22 -0500

    active indicator for parent and child

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,19 +20,21 @@ export default function Sidebar({ links }: SidebarProps) {
   return (
     <ul className="-mt-1">
       {links.map(link => {
-        const isParentActive = pathname === link.path;;
+        const isParentActive = pathname === link.path;
+        const childPaths = link.children?.map(({ path }) => path);
+        const hasActiveChild = childPaths?.includes(pathname);
         return (
           <li key={`${link.label}-${link.path}`} className="mb-1">
             <Link
               className={cn(
                 'block font-semibold px-6 py-2',
-                isParentActive && 'text-white bg-blue-500'
+                (isParentActive || hasActiveChild) && 'text-white bg-blue-500'
               )} href={link.path}
             >
               { link.label }
             </Link>
             {link.children && link.children.length > 0 && (
-              <ul className="pl-2">
+              <ul className="pl-2 my-1">
                 { link.children.map(child => {
                   const isChildActive = pathname === child.path;
                   return (
